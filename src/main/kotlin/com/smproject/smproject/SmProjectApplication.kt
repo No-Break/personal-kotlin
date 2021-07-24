a47098555cb5f1edad665a5df87fce1f10701d88
seongmin <qowlxj08@naver.com>
2021-07-24 10:55:24 +0900

    first commit

---
+++ src/main/kotlin/com/smproject/smproject/SmProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.smproject.smproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/smproject/smproject/SmProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smproject.smproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
